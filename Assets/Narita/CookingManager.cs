8c6ced9d9f6f1d8b9972182ecd0433f186e36324
NaritaShosei <onigirikun7171@gmail.com>
2025-08-04 17:21:07 +0900

    [update]レシピの保存

---
+++ Assets/Narita/CookingManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CookingManager : MonoBehaviour
+{
+    private List<(RecipeData recipe, string name)> _recipes;
+    public List<(RecipeData recipe, string name)> Recipes => _recipes;
+
+    public void AddRecipe((RecipeData recipe, string name) recipe)
+    {
+        _recipes.Add(recipe);
+    }
+
+}
