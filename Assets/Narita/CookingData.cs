cbebe96754c0a29c9629dc0187ba7c8508c5bd34
NaritaShosei <onigirikun7171@gmail.com>
2025-08-04 14:11:46 +0900

    [add]料理のデータ

---
+++ Assets/Narita/CookingData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class CookingData
+{
+    [SerializeField] private string _foodName = "";
+    [SerializeField] private RecipeData _recipeData;
+    // TODO:ここはコマンドのデータ
+
+    public string FoodName => _foodName;
+    public RecipeData RecipeData => _recipeData;
+}
