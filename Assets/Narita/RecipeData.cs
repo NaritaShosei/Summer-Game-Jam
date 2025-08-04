b7aa4fe2c1519957584ca5ce7379507a3f5b51f0
NaritaShosei <onigirikun7171@gmail.com>
2025-08-04 14:11:33 +0900

    [add]レシピのデータ

---
+++ Assets/Narita/RecipeData.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class RecipeData
+{
+    [SerializeField] private string[] _foods;
+    [SerializeField] private CookingType _cookingType;
+    public string[] Foods => _foods;
+    public CookingType CookingType => _cookingType;
+}
+public enum CookingType
+{
+    // 生
+    None = 0,
+    // 煮る
+    Simmer = 1,
+    // 焼く
+    Bake = 2,
+    // 揚げる
+    Fire = 3,
+}
