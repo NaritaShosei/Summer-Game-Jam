70021e2597f3dc05bccec912c3ad92df0c14cd15
NaritaShosei <onigirikun7171@gmail.com>
2025-08-05 10:26:08 +0900

    [add]ImageのData

---
+++ Assets/Narita/ImageDataBase.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ImageData/DataBase", fileName = "ImageDataBase")]
+public class ImageDataBase : ScriptableObject
+{
+    [SerializeField] private ImageData[] _imageData;
+    [SerializeField, Header("煮るの画像")] private Sprite _simmerImage;
+    [SerializeField, Header("焼くの画像")] private Sprite _bakeImage;
+    [SerializeField, Header("揚げ物の画像")] private Sprite _fryImage;
+
+    public Sprite GetSprite(string name)
+    {
+        foreach (var item in _imageData)
+        {
+            if (item.Image.name == name)
+            {
+                return item.Image.sprite;
+            }
+        }
+        return null;
+    }
+
+    public Sprite GetSprite(CookingMethod type)
+    {
+        return type switch
+        {
+            CookingMethod.Simmer => _simmerImage,
+            CookingMethod.Bake => _bakeImage,
+            CookingMethod.Fry => _fryImage,
+            _ => null // 予期しない値には null を返す（例外防止）
+        };
+    }
+
+}
---
+++ Assets/Narita/ImageManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageManager : MonoBehaviour
+{
+    [SerializeField] private ImageDataBase _dataBase;
+    public ImageDataBase DataBase => _dataBase;
+}
